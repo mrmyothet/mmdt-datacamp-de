3dc2fbaa5f65cf597e4e0fe063c166895b5856d1
Myo Thet <mr.myothet@gmail.com>
2025-03-12 23:09:00 +0700

    software engineering and data science

---
+++ se_principles/se_data_science/Counter_mostcommon.py
@@ -0,0 +1,13 @@
+from data import words
+
+# load the Counter function into our environment
+from collections import Counter
+
+# View the documentation for Counter.most_common
+# help(Counter.most_common)
+
+# use Counter to find the top 5 most common words
+top_5_words = Counter(words).most_common(5)
+
+# display the top 5 most common words
+print(top_5_words)
---
+++ se_principles/se_data_science/using_help.py
@@ -0,0 +1,5 @@
+# load the Counter function into our environment
+from collections import Counter
+
+# View the documentation for Counter.most_common
+help(Counter.most_common)
