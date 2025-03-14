12df02a293ef110dfef8cfc27d064ce0b4b5261c
Myo Thet <mr.myothet@gmail.com>
2025-03-14 22:42:08 +0700

    testing - doctest and pytest

--- se_principles/python_module/text_analyzer/social_media.py
+++ se_principles/python_module/text_analyzer/social_media.py
@@ -3,6 +3,14 @@ from collections import Counter
 
 
 class SocialMedia(Document):
+    """Analyze text data from social media
+
+    :param text: social media text to analyze
+
+    :ivar hashtag_counts: Counter object containing counts of hashtags used in text
+    :ivar mention_counts: Counter object containing counts of @mentions used in text
+    """
+
     def __init__(self, text):
         super().__init__(text=text)
 
