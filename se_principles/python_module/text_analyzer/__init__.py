221b3a5b3a36f703517c11b169013d8b2f5a1d9c
Myo Thet <mr.myothet@gmail.com>
2025-03-14 16:22:04 +0700

    add SociaMedia class

--- se_principles/python_module/text_analyzer/__init__.py
+++ se_principles/python_module/text_analyzer/__init__.py
@@ -1,3 +1,4 @@
 from .counter_utils import plot_counter, sum_counters
 
 from .document import Document
+from .social_media import SocialMedia
---
+++ se_principles/python_module/text_analyzer/social_media.py
@@ -0,0 +1,31 @@
+from .document import Document
+from collections import Counter
+
+
+class SocialMedia(Document):
+    def __init__(self, text):
+        self.text = text
+        Document.__init__(self, text)
+
+        self.hashtag_counts = self._count_hashtags()
+        self.mention_counts = self._count_mentions()
+
+    def _count_hashtags(self):
+        # Filter attribute so only words starting with '#' remain
+        return filter_word_counts(self.word_counts, first_char="#")
+
+    def _count_mentions(self):
+        # Filter attribute so only words starting with '@' remain
+        return filter_word_counts(self.word_counts, first_char="@")
+
+
+def filter_word_counts(word_counts, first_char):
+    """
+    Filter Document.word_counts by the first character of the word
+    :param word_counts: the word_counts attribute of a Document class instance
+    :param first_char: only keep word counts that start with this character
+    >>> # How to filter to only words that start with \'A\
+    >>> filter_word_counts(document.word_counts, \'A\')\n    
+    """
+
+    return Counter({k: v for k, v in word_counts.items() if k[0] == first_char})
