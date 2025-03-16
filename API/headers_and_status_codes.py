4935ee55778a3c3ae8f4a0e45695fcc54ce1f783
Myo Thet <mr.myothet@gmail.com>
2025-03-16 19:45:46 +0700

    headers and status codes

--- API/headers_and_status_codes.py
+++ API/headers_and_status_codes.py
@@ -1,4 +1,5 @@
-import requests, os
+import requests
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,11 +11,31 @@ city_name = "Yangon"
 api_key = os.getenv("open_weather_api_key")
 
 query_params = {"q": city_name, "appid": api_key}
+# headers = {"accept": "application/json"}
+headers = {"accept": "application/xml"}
 
-response = requests.get(url, params=query_params)
+response = requests.get(url, headers=headers, params=query_params)
 
 # Check the response status code
-if response.status_code == 200:
+if response.status_code == requests.codes.ok:
     print("The server responded succesfully!")
-elif response.status_code == 404:
+elif response.status_code == requests.codes.not_found:
     print("Oops, that API could not be found!")
+
+# Check if the server did not accept the request
+if response.status_code == 406:
+    print("The server can not respond in XML")
+
+    # Print the accepted content types
+    print(
+        "These are the content types the server accepts: "
+        + response.headers.get("accept")
+    )
+else:
+    print(response.text)
+
+# Print the response content-type header
+print(response.headers.get("content-type"))
+
+# Print the response accept header
+print(response.headers.get("accept"))
