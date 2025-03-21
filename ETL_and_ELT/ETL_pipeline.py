cde4899463f001ed5a1baf61ee0023fa61f05014
Myo Thet <mr.myothet@gmail.com>
2025-03-21 22:28:04 +0700

    extract and transform

--- ETL_and_ELT/ETL_pipeline.py
+++
@@ -1,8 +0,0 @@
-from ETL_functions import extract, transform, load
-
-
-extracted_data = extract(file_name="raw_sales_data.csv")
-load(data_frame=extracted_data, table_name="raw_sales_data")
-
-# Populate total_sales by transforming raw_sales_data
-transform(source_table="raw_sales_data", target_table="total_sales")
