a704c50b13d55e82285854ccb611a1c724b71558
Myo Thet <mr.myothet@gmail.com>
2025-04-12 21:04:12 +0700

    add other operators and define the flow of the tasks

--- apache_airflow/dags/test_dag.py
+++ apache_airflow/dags/test_dag.py
@@ -18,3 +18,18 @@ with DAG(dag_id="test_dag", default_args=default_args) as analytics_dag:
 
     # Define a final operator to execute the `push_data.sh` script
     push_data = BashOperator(task_id="push_data", bash_command="push_data.sh")
+
+    # Define a new pull_sales task
+    pull_sales = BashOperator(
+        task_id="pullsales_task",
+        bash_command="wget https://salestracking/latestinfo?json",
+    )
+
+    # Set pull_sales to run prior to cleanup
+    pull_sales >> cleanup
+
+    # Configure consolidate to run after cleanup
+    cleanup >> consolidate
+
+    # Set push_data to run last
+    consolidate >> push_data
